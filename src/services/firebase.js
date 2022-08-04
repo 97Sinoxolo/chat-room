58707e2cb93e67110c0049ce65f203325983f734
97Sinoxolo <snoxy.sooi@gmail.com>
2022-08-04 14:37:55 +0200

    first commit

---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, signInWithPopup, getAuth} from 'firebase/auth';
+const firebaseConfig = {
+    // TODO: Add your Firebase configuration here
+};
+const app = initializeApp(firebaseConfig);
+
+async  function loginWithGoogle(){
+    try { 
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+
+        const {user} = await signInWithPopup(auth, provider);
+
+        return {uid: user.uid, displayName: user.displayName};
+    } catch (error) {
+        if (error.code !== 'auth/cancelled-popup-request') {
+            console.error(error);
+        }
+
+        return null;
+    }
+}
+
+export {loginWithGoogle};
