42723b0499ee696f6af7c08a53b344772474b3d4
97Sinoxolo <snoxy.sooi@gmail.com>
2022-08-14 19:02:31 +0200

    message  send done

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,25 +1,52 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, signInWithPopup, getAuth} from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+
 const firebaseConfig = {
-    // TODO: Add your Firebase configuration here
+  apiKey: "AIzaSyB9hU5qk4-OxY1qTUR7_Ahv4d7TaNmq-f0",
+  authDomain: "chat-room-3aea4.firebaseapp.com",
+  projectId: "chat-room-3aea4",
+  storageBucket: "chat-room-3aea4.appspot.com",
+  messagingSenderId: "830820825197",
+  appId: "1:830820825197:web:75a70c48d5dd6508aee764",
 };
+
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-async  function loginWithGoogle(){
-    try { 
-        const provider = new GoogleAuthProvider();
-        const auth = getAuth();
+async function loginWithGoogle() {
+  try {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
 
-        const {user} = await signInWithPopup(auth, provider);
+    const { user } = await signInWithPopup(auth, provider);
 
-        return {uid: user.uid, displayName: user.displayName};
-    } catch (error) {
-        if (error.code !== 'auth/cancelled-popup-request') {
-            console.error(error);
-        }
-
-        return null;
+    return { uid: user.uid, displayName: user.displayName };
+  } catch (error) {
+    if (error.code !== "auth/cancelled-popup-request") {
+      console.error(error);
     }
+
+    return null;
+  }
 }
 
-export {loginWithGoogle};
+async function sendMessage(roomId, user, text) {
+  try {
+    await addDoc(collection(db, 'chat-rooms', roomId, 'messages'), {
+      uid: user.id,
+      displayName: user.displayName,
+      text: text.trim(),
+      timestamp: serverTimestamp(),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { loginWithGoogle, sendMessage };
